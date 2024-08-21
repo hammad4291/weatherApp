71001abd6b33b0e28a68798663e54e6f952168bf
hammad4291 <www.hammad622h@gmail.com>
2024-08-21 18:40:20 +0500

    UI completed

--- weather/src/weather/temp.js
+++ weather/src/weather/temp.js
@@ -15,6 +15,69 @@ const Temp = () => {
         Search</button>
         </div>
       </div>
+
+        {/* temp card */}
+        <article className="widget">
+            <div className="weatherIcon">
+                <i className={"wi wi-day-sunny"}></i>
+            </div>
+            <div className="weatherInfo">
+                <div className="temperature">
+                    <span>25.5&deg;</span>
+                </div>
+                <div className="description">
+                    <div className="weatherCondition">
+                        Cloudy
+                    </div>
+                    <div className="place">Rawalpindi, Pakistan</div>
+                </div>
+            </div>
+            <div className="date">{new Date().toLocaleString()}</div>
+            {/* 4 column section */}
+            <div className="extra-temp">
+                <div className="temp-info-minmax">
+                <div className="two-sided-section">
+                    <p>
+                        <i className={"wi wi-sunset"}></i>
+                    </p>
+                    <p className='extra-info-leftside'>
+                    19:19 PM <br />
+                    Sunset
+                    </p>
+                    </div>
+                    <div className="two-sided-section">
+                    <p>
+                        <i className={"wi wi-humidity"}></i>
+                    </p>
+                    <p className='extra-info-leftside'>
+                    19:19 PM <br />
+                    Humidity
+                    </p>
+                    </div>
+                </div>
+                <div className="weather-extra-info">
+                <div className="two-sided-section">
+                    <p>
+                        <i className={"wi wi-rain"}></i>
+                    </p>
+                    <p className='extra-info-leftside'>
+                    19:19 PM <br />
+                    Pressure
+                    </p>
+                    </div>
+                    <div className="two-sided-section">
+                    <p>
+                        <i className={"wi wi-strong-wind"}></i>
+                    </p>
+                    <p className='extra-info-leftside'>
+                    19:19 PM <br />
+                    Speed
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </article>
+
     </>
   )
 }
